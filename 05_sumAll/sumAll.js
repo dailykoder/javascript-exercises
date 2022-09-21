6c31f2a1e38da4ae8bb7de86e3afd20d90a9bb8c
kjcha0611@gmail.com <dailykoderkjc@gmail.com>
2022-09-21 13:22:41 -0700

    finished 06_leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    //check if either number is not a number, return error if so
+    //check if either number is less than 0, return error if so
+    //make sure the starting number is less than the end number
+    //loop through the start to end numbers and add to sum total
+    //return sum
+    if(checkNumberValidity(startNum) && checkNumberValidity(endNum)) {
+        let sum = 0;
+        if(startNum > endNum) {
+            for(endNum; endNum <= startNum; endNum++) {
+                sum += endNum;
+            }
+        } else {
+            for(startNum; startNum <= endNum; startNum++) {
+                sum += startNum;
+            }
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
+
+const checkNumberValidity = function(num) {
+    if(typeof num === 'number' && num >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
