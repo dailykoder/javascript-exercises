38bd60d68494b9e6ec5e8074d5e0f87fd6064ad7
kjcha0611@gmail.com <dailykoderkjc@gmail.com>
2022-09-21 12:07:10 -0700

    finished 03_reverse_string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //take a str input, split it to an array of letters
+    //access end of array and put into another array
+    //join array back into string using join()
+    let splitStr = str.split('');
+    let reversedArr = [];
+    let counter = splitStr.length - 1;
+    for(counter; counter >= 0; counter--) {
+        reversedArr.push(splitStr[counter]);
+    }
+    return reversedArr.join('');
 };
 
 // Do not edit below this line
