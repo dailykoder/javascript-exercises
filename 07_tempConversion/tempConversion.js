0b3d2c2abf1db54ff0154b9df3bdcbf6813ce68b
kjcha0611@gmail.com <dailykoderkjc@gmail.com>
2022-09-21 13:51:19 -0700

    finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = ((temp - 32.0) * 5/9);
+  return Math.round(celsius * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let faren = ((temp * 9/5) + 32.0);
+  return Math.round(faren * 10)/10;
 };
 
 // Do not edit below this line
