3e43161679c391d54a499b31584fa61617ed4f02
kjcha0611@gmail.com <dailykoderkjc@gmail.com>
2022-09-21 16:11:27 -0700

    finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  arr.forEach(num => total += num);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(num => product *= num);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  return Math.pow(num, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  if(num <= 1) {
+    return 1;
+  } else {
+    for(num; num > 0; num--) {
+      product *= num;
+    }
+  }
+  return product;
 };
 
 // Do not edit below this line
