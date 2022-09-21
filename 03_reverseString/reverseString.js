017329d53dd8ffc2aefa789b005722f26021020d
kjcha0611@gmail.com <dailykoderkjc@gmail.com>
2022-09-21 13:02:47 -0700

    finished 04_reverseStrng

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,9 @@
-const reverseString = function(str) {
+const reverseString = function(...args) {
     //take a str input, split it to an array of letters
     //access end of array and put into another array
     //join array back into string using join()
-    let splitStr = str.split('');
-    let reversedArr = [];
-    let counter = splitStr.length - 1;
-    for(counter; counter >= 0; counter--) {
-        reversedArr.push(splitStr[counter]);
-    }
-    return reversedArr.join('');
+    let array = args[0];
+    array.filter((num) => !args.includes(num);)
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    //loop through arguments array
+    //check if index exists in main array with indexOf()
+    //splice the elements from the args array
+    //return new removed array
+    if(args.length === 1) { 
+        let index = arr.indexOf(args[0]);
+        if(index > -1) {
+            arr.splice(index, 1);
+            return arr 
+        } else {
+            return arr
+        }
+    } else {
+        for(let i = 0; i < args.length; i++) {
+            let index = arr.indexOf(args[i]);
+            if(index > -1) {
+                arr.splice(index, 1);
+            }
+        }
+        return arr
+    }
 };
 
 // Do not edit below this line
